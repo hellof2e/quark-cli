06c91fdc04a80298c802e3845e4f332d4f467c75
xushunfa459 <xushunfa459@hellobike.com>
2023-03-20 16:51:30 +0800

    feat: 更换底层核心库

--- template/demo/src/main.tsx
+++ template/demo/src/main.tsx
@@ -1,41 +1,41 @@
-import QuarkElement, { customElement, property, state } from '@quarkd/core'
-import style from './style.css'
+import { QuarkElement, Fragment, property, customElement } from "quarkc";
+import logo from "./quark.png";
+import style from "./main.css";
 
-@customElement({
-  tag: 'my-component',
-  style,
-})
-export default class MyComponent extends QuarkElement {
-  @state() counter: number = 0
-
-  @property() atitle: string = ''
-
-  componentDidMount() {
-    console.log(this.atitle, 333)
+@customElement({ tag: "my-element", style })
+class MyElement extends QuarkElement {
+  @property({
+    type: Number
+  })
+  count = 0;
+  
+  add = () => {
+    this.count += 1;
+    console.log('button was clicked', typeof this.count);
   }
-
-  handleClick = () => {
-    this.counter++
-  }
-
+  
   render() {
     return (
-      <div>
-        <a href='https://quark-design.hellobike.com' target='_blank'>
-          <img
-            src='https://m.hellobike.com/resource/helloyun/12395/0USLE_quark.png?x-oss-process=image/quality,q_80'
-            className='logo vanilla'
-            alt='JavaScript logo'
-          />
-        </a>
-        <h1>{this.atitle}</h1>
-        <div class='card'>
-          <button onClick={this.handleClick} type='button'>
-            count is {this.counter}
-          </button>
+      <Fragment>
+        <div>
+          <a href="https://quark-design.hellobike.com" target="_blank">
+            <img src={logo} class="logo" alt="quark logo" />
+          </a>
         </div>
-        <p class='read-the-docs'>Click on the Quark logo to learn more</p>
-      </div>
-    )
+        
+        <h1>Quark - 为跨技术栈而生</h1>
+
+        <div className="card">
+          <button onClick={this.add}>
+            count is: { this.count }
+          </button>
+          <p>
+            Edit <code>src/app.tsx</code> and save to test HMR
+          </p>
+        </div>
+      </Fragment>
+    );
   }
 }
+
+export default my-element;
