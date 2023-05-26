d3f80ae8174ec9ad61b33a59a6a0cea9a852bf6f
xsf0105 <allan91coder@gmail.com>
2023-05-26 14:28:02 +0800

    feat: remove px 2 rem

--- template/application/component/rollup.config.mjs
+++ template/application/component/rollup.config.mjs
@@ -3,7 +3,6 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import postcss from '@quarkd/rollup-plugin-postcss';
-import px2vp from "postcss-px-to-viewport";
 import filesize from 'rollup-plugin-filesize';
 
 const extensions = ['.js', '.ts', '.tsx'];
@@ -22,23 +21,6 @@ const options = [
     treeshake: false,
     plugins: [
       postcss({
-        plugins: [
-          px2vp({
-            unitToConvert: 'px',
-            viewportWidth: 375,
-            unitPrecision: 5,
-            propList: ['*'],
-            viewportUnit: 'vw',
-            fontViewportUnit: 'vw',
-            selectorBlackList: [],
-            minPixelValue: 1,
-            mediaQuery: false,
-            replace: true,
-            exclude: undefined,
-            include: undefined,
-            landscape: false,
-          })
-        ],
         inject: false,
         extensions: ['.css'],
       }),
