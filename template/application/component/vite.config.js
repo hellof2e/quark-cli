9e28164aabd0c465294b816676562e7a69bb64d5
xsf0105 <allan91coder@gmail.com>
2023-05-30 11:14:03 +0800

    feat: add vite plugin for css

--- template/application/component/vite.config.js
+++ template/application/component/vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import FullReload from 'vite-plugin-full-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: []
+  plugins: [
+    FullReload(['src/**/*.css'])
+  ]
 });
